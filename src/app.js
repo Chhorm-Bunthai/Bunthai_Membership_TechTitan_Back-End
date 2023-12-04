6399aa85af51318bf6d30c6b48aba150f78b0b5f
RelexSun <relexsunnop@gmail.com>
2023-12-04 09:11:06 +0700

    feat: global error controller

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+
 const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
