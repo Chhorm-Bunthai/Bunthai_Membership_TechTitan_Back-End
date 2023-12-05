5912113c891e511ba032939d2b494af4244aabf3
Your Name <bunthaicambo89@gmail.com>
2023-12-05 18:21:22 +0700

    already get data from users

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const userRoutes = require("./routes/userRoutes");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
+app.use(express.json());
+app.use("/api/users", userRoutes);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-const userController = require('../controllers/userController');
+const express = require("express");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+router.post("/signup", authController.signup);
 
+module.exports = router;
