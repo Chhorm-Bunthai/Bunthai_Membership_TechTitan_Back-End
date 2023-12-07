52048b9e22d1a91d49d6bcc3fff60e39b5d02c5b
Your Name <bunthaicambo89@gmail.com>
2023-12-07 16:22:50 +0700

    feature: Signup.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
+const userRoutes = require("./routes/userRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: "GET,POST,PATCH,PUT,DELETE,HEAD",
+  credential: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use("/api/users", userRoutes);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
