413efa03da3fd47e8314a1b143c609d1211ace22
RelexSun <relexsunnop@gmail.com>
2023-12-07 11:15:03 +0700

    feat: development and production errors

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -2,8 +2,26 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  };
+
+  const sendErrorProd = (err, res) => {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  };
+
+  // development vs production errors
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
