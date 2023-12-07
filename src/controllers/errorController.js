565ca104bf1fd2edea72bd62f6521c1418fabed0
RelexSun <relexsunnop@gmail.com>
2023-12-07 11:33:09 +0700

    chore: managing error for development and production

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,22 +1,31 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // only send generic errors to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // in case of any errors that occur by programming errors or an unknown errors
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong",
+    });
+  }
+};
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-
-  const sendErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  };
-
-  const sendErrorProd = (err, res) => {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  };
 
   // development vs production errors
   if (process.env.NODE_ENV === "development") {
